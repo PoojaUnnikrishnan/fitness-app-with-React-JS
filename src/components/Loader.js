72dbfa3709017d1ff37022400ca413f7f9edf484
pooja unnikrishnan <poojaunnikrishnan7558@gmail.com>
2022-07-06 20:18:11 +0530

    components

---
+++ src/components/Loader.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Stack } from "@mui/material";
+import { Circles } from "react-loader-spinner";
+const Loader = () => (
+  <Stack
+    direction="row"
+    justifyContent="center"
+    alignItems="center"
+    width="100%"
+  >
+    <Circles color="#00BFFF" height={80} width={80} />
+  </Stack>
+);
+
+export default Loader;
