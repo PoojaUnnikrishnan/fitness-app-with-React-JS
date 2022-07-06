f3a27b62e946b057b266185632339af4cebee0c8
pooja unnikrishnan <poojaunnikrishnan7558@gmail.com>
2022-07-06 11:05:51 +0530

    component

---
+++ src/components/SearchExercises.js
@@ -0,0 +1,92 @@
+import React, { useEffect, useState } from "react";
+import { Button, Box, Stack, TextField, Typography } from "@mui/material";
+import { exerciseOptions, fetchData } from "../utils/fetchData";
+import HorizontalScrollbar from "./HorizontalScrollbar";
+const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
+  const [search, setSearch] = useState("");
+  const [bodyParts, setBodyParts] = useState([]);
+  useEffect(() => {
+    const fetchExrcisesData = async () => {
+      //The exercises are fetched from url.
+      const bodyPartsData = await fetchData(
+        "https://exercisedb.p.rapidapi.com/exercises/bodyPartList",
+        exerciseOptions
+      );
+      setBodyParts(["all", ...bodyPartsData]);
+    };
+    fetchExrcisesData();
+  }, []);
+  const handleSearch = async () => {
+    if (search) {
+      const exercisesData = await fetchData(
+        "https://exercisedb.p.rapidapi.com/exercises",
+        exerciseOptions //Exercise options authorise with secret key and fetch data from api.
+      );
+      const searchedExercises = exercisesData.filter(
+        //searching for different exercises according to what user types.
+        (exercise) =>
+          exercise.name.toLocaleLowerCase().includes(search) ||
+          exercise.target.toLocaleLowerCase().includes(search) ||
+          exercise.equipment.toLocaleLowerCase().includes(search) ||
+          exercise.bodyPart.toLocaleLowerCase().includes(search)
+      );
+      // window.scrollTo({ top: 1800, left: 100, behavior: "smooth" });
+      setSearch(""); //first serach is empty
+      setExercises(searchedExercises); //search is given the input it gets by user
+    }
+  };
+  return (
+    <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
+      <Typography
+        fontWeight={700}
+        sx={{ fontSize: { lg: "44px", xs: "30px" } }}
+        mb="50px"
+        textAlign="center"
+      >
+        Awsome Exercises You <br />
+        Should Know
+      </Typography>
+      <Box position="relative" mb="72px">
+        <TextField
+          sx={{
+            input: { fontWeight: "700", border: "none", borderRadius: "4px" },
+            width: { lg: "800px", xs: "350px" },
+            backgroundColor: "#fff",
+            borderRadius: "40px",
+          }}
+          hight="76px"
+          value={search}
+          onChange={(e) => setSearch(e.target.value.toLocaleLowerCase())}
+          placeholder="Search Exercise"
+          type="text"
+        />
+        <Button
+          onClick={handleSearch}
+          className="search-btn"
+          sx={{
+            bgcolor: "#FF2625",
+            color: "#fff",
+            textTransform: "none",
+            width: { lg: "173px", xs: "80px" },
+            height: "56px",
+            position: "absolute",
+            right: "0px",
+            fontSize: { lg: "20px", xs: "14px" },
+          }}
+        >
+          Search
+        </Button>
+      </Box>
+      <Box sx={{ position: "relative", width: "100%", p: "20px" }}>
+        <HorizontalScrollbar
+          data={bodyParts}
+          bodyParts
+          setBodyPart={setBodyPart}
+          bodyPart={bodyPart}
+        />
+      </Box>
+    </Stack>
+  );
+};
+
+export default SearchExercises;
