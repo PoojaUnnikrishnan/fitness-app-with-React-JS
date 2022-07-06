38bd7192943bebeae573dee20fee0d20a6cbc2f6
pooja unnikrishnan <poojaunnikrishnan7558@gmail.com>
2022-07-06 11:06:43 +0530

    fetching data from rapi api

---
+++ src/utils/fetchData.js
@@ -0,0 +1,14 @@
+//Make a connection to rapid api.
+export const exerciseOptions = {
+  method: "GET",
+
+  headers: {
+    "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY, //api key from rapid api exerciseDB.
+  },
+};
+export const fetchData = async (url, options) => {
+  const response = await fetch(url, options);
+  const data = await response.json();
+  return data;
+};
