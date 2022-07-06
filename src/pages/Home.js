d55f8ee65de738c6e5d8b4fec53743769777ad09
pooja unnikrishnan <poojaunnikrishnan7558@gmail.com>
2022-07-06 11:06:09 +0530

    pages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,27 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { Box } from "@mui/material";
+import Exercises from "../components/Exercises";
+import SearchExercises from "../components/SearchExercises";
+import HeroBanner from "../components/HeroBanner";
 const Home = () => {
-  return <div>Home</div>;
+  const [exercises, setExercises] = useState([]);
+  const [bodyPart, setBodyPart] = useState("all");
+
+  return (
+    <Box>
+      <HeroBanner />
+      <SearchExercises
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
+      <Exercises
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        exercises={exercises}
+      />
+    </Box>
+  );
 };
 
 export default Home;
